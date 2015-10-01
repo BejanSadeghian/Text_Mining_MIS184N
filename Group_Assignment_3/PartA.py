177cf0ab9f14bef54a4c9b2161c8a3ba19d17fd4
Bejan Lee Sadeghian <bejan.sadeghian@gmail.com>
2015-09-30 20:55:28 -0500

    Part A Done Part B Work in Progress

--- Group_Assignment_3/PartA.py
+++ Group_Assignment_3/PartA.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 
 
 rawsentiment = pd.read_csv(r'C:\Users\beins_000\Documents\GitHub\Text_Mining_MIS184N\Group_Assignment_3\Assignment 3 Sentiment Scores (2).csv')
@@ -57,23 +58,129 @@ G=nx.DiGraph()
 
 #Creating the labels for the nodes and adding a node to the G object
 labels = {}
+opp_labels = {}
 for i, value in enumerate(names):
-    labels[i] = value
+    labels[i] = str(value)
+    opp_labels[value] = i
     G.add_node(i) #Adding the nodes
+    
 
-G.add_weighted_edges_from([(1,2,0.5),(3,2,1.75)])
+#Create the list of tuples for edges
+node_list = []
+for index, pref in enumerate(averages_df.loc[:,'Positive']): #Positives
+    if ~np.isnan(pref):
+        node_list.append((pd.Series(averages_df.loc[:,'Node 1'])[index],pd.Series(averages_df.loc[:,'Node 2'])[index]))
+        G.add_edge(*(opp_labels[pd.Series(averages_df.loc[:,'Node 1'])[index]],opp_labels[pd.Series(averages_df.loc[:,'Node 2'])[index]]))
+for index, pref in enumerate(averages_df.loc[:,'Negative']): #Negatives
+    if ~np.isnan(pref):
+        node_list.append((pd.Series(averages_df.loc[:,'Node 2'])[index],pd.Series(averages_df.loc[:,'Node 1'])[index]))
+        G.add_edge(*(opp_labels[pd.Series(averages_df.loc[:,'Node 2'])[index]],opp_labels[pd.Series(averages_df.loc[:,'Node 1'])[index]]))
 
-pos = nx.spring_layout(G)
 
-G.add_node(1)
-G.add_node(2)
-G.add_node(3)
-G.add_edge(1,2, weight = 5)
+pos = nx.circular_layout(G) #Positions
 
-G.add_edge(2,1, weight = 5)
+#nx.edges([(1,2),(1,0)])#node_list)
 
 nx.draw_networkx_labels(G,pos, labels, font_size=16)
-nx.draw_networkx_nodes(G,pos,node_color='skyblue')
-plt.figure(num=None, figsize=(10,9))
-plt.show()
+#nx.draw_networkx_nodes(G,pos,node_color='skyblue')
+nx.draw(G,pos,node_size=1000)
+###nx.Graph(node_list)
 
+fig1 = plt.gcf()
+plt.figure(num=None, figsize=(30,20))
+#plt.savefig('network.png', dpi=100)
+
+plt.show()
+fig1.savefig('network.png', dpi=1000)
+
+
+##Part B
+
+#Unweighted Page Rank
+unweighted_PR = nx.pagerank(G, alpha = 0.5)
+
+#Weighted Page Rank
+##Creating the network graph
+FG=nx.Graph()
+
+#Creating the labels for the nodes and adding a node to the G object
+labels = {}
+opp_labels = {}
+for i, value in enumerate(names):
+    labels[i] = str(value)
+    opp_labels[value] = i
+    FG.add_node(value) #Adding the nodes
+#Create the list of tuples for edges with weights this time
+node_list = []
+for index, pref in enumerate(averages_df.loc[:,'Positive']): #Positives
+    if ~np.isnan(pref):
+        node_list.append((pd.Series(averages_df.loc[:,'Node 1'])[index],pd.Series(averages_df.loc[:,'Node 2'])[index],pref))
+FG.add_weighted_edges_from(node_list)
+
+
+for index, pref in enumerate(averages_df.loc[:,'Negative']): #Negatives
+    if ~np.isnan(pref):
+        node_list.append((pd.Series(averages_df.loc[:,'Node 2'])[index],pd.Series(averages_df.loc[:,'Node 1'])[index],pref))
+FG.add_weighted_edges_from(node_list)
+
+
+weighted_PR = nx.pagerank(FG, alpha = 0.5, weight = 'weight')
+
+
+
+
+
+
+
+
+
+
+
+
+PR_ES = 1
+PR_LS = 1
+PR_RX =1
+PR_A8 =1
+PR_A6 = 1
+PR_3series = 1
+PR_5series = 1
+PR_7series = 1
+PR_XJ = 1
+PR_Sclass = 1
+
+#Count the number of 
+ref = {}
+for n in names:
+    count = int(0)
+    count = [(count + 1) for (y, x) in node_list if x == n]
+    ref[n] = sum(count)
+RF_ES = float(1)/ref['ES']
+RF_LS = float(1)/ref['LS']
+RF_RX =float(1)/ref['RX']
+RF_A8 =float(1)/ref['A8']
+RF_A6 = float(1)/ref['A6']
+RF_3series = float(1)/ref['3series']
+RF_5series = float(1)/ref['5series']
+RF_7series = float(1)/ref['7series']
+RF_XJ = float(1)/ref['XJ']
+RF_Sclass = float(1)/ref['Sclass']
+
+
+
+PR_ES = 0.5 + 0.5*((PR_LS/RF_LS) + (PR_RX/RF_RX) + (PR_A8/RF_A8)
+
+
+
+
+
+
+ES = RF_ES
+LS = RF_LS
+RX = RF_RX
+A8 = RF_A8
+A6 = RF_A6
+3series = RF_3series
+5series = RF_5series
+7series = RF_7series
+XJ = RF_XJ
+Sclass = RF_Sclass
